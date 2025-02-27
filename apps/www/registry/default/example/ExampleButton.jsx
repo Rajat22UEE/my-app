abc60c895861ca9bb8a29b4900320d5451e889ed
Rajat22UEE <rajatnath6909@gmail.com>
2025-02-27 10:27:19 +0530

    added much

---
+++ apps/www/registry/default/example/ExampleButton.jsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button";
+
+export default function ExampleButton() {
+  return (
+    <Button variant="default" size="default">
+      Example Button
+    </Button>
+  );
+}
